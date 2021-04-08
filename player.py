883c5d831b914be7b76b193c23e8014e93c4d247
luanbrito <luan@viitra.com>
2021-04-08 20:29:49 -0300

    mout audio chunks using pydub

---
+++ player.py
@@ -0,0 +1,18 @@
+import json
+from pydub import AudioSegment
+
+f = open('results/map.json')
+sync_result = json.load(f)
+
+song = AudioSegment.from_wav("data/the_18th_letter.wav")
+secs = 1000
+chunks = []
+for fragment in sync_result["fragments"]:
+  print(fragment)
+  chunk = AudioSegment.empty()
+  chunk = song[ fragment["begin"] * secs: fragment["end"] * secs]
+  chunks.append({
+    'chunk': chunk,
+    'lines': fragment["lines"]
+  })
+
