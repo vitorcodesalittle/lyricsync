8822d0245ed6e5e6ca0a03f3c79025d73c4a8c27
vilma <vlma@cin.ufpe.br>
2021-04-10 17:48:59 -0300

    add empty fragments check on player

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from pydub import AudioSegment
 from pydub.playback import play
 
@@ -11,13 +12,24 @@ chunks = []
 for fragment in sync_result["fragments"]:
   print(fragment)
   chunk = AudioSegment.empty()
-  chunk = song[ float(fragment["begin"]) * secs: float(fragment["end"]) * secs]
+  begin = fragment["begin"]
+  end = fragment["end"]
+  if begin == end:
+      print("Jumping fragment because it's empty")
+      continue
+  chunk = song[ float(begin) * secs: float(end) * secs]
   chunks.append({
     'chunk': chunk,
     'lines': fragment["lines"]
   })
   print(fragment["lines"])
-  play(chunk)
+  try:
+      play(chunk)
+  except Exception:
+      print("Error playing audio chunk")
+      traceback = sys.exc_info()[2]
+      print(sys.exc_info()[1])
+      print(traceback.print_tb())
 
 
 
